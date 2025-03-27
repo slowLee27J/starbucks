b5ddaec063f15e773e36d224946e9c005dfdedd9
slowLee27J <drill27j@naver.com>
2025-03-27 14:15:06 +0900

    벳지 이미지 수정

--- js/main.js
+++ js/main.js
@@ -20,14 +20,39 @@ const toTopEl = document.querySelector('#to-top');
 
 // lodash cdn
 // _.throttle(함수, 시간)
-window.addEventListener('scroll', _.throttle(function() {
+window.addEventListener('scroll', _.throttle(function () {
+  // 페이지 스크롤 위치가 500px이 넘으면.
   if (window.scrollY > 500) {
-    // 배지 숨기기
-    // gsap.to(요소, 지속시간(초단위), 옵션);
+    // Badge 요소 숨기기!
     gsap.to(badgeEl, .6, {
       opacity: 0,
       display: 'none'
-    });
+    })
+    // 상단으로 스크롤 버튼 보이기!
+    gsap.to(toTopEl, .2, {
+      x: 0
+    })
+
+  // 페이지 스크롤 위치가 500px이 넘지 않으면.
+  } else {
+    // Badge 요소 보이기!
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    })
+    // 상단으로 스크롤 버튼 숨기기!
+    gsap.to(toTopEl, .2, {
+      x: 100
+    })
+  }
+}, 300))
+// 상단으로 스크롤 버튼을 클릭하면,
+toTopEl.addEventListener('click', function () {
+  // 페이지 위치를 최상단으로 부드럽게(0.7초 동안) 이동.
+  gsap.to(window, .7, {
+    scrollTo: 0
+  })
+});
 
 const fadeEls = document.querySelectorAll('.visual .fade-in');
 fadeEls.forEach(function (fadeEl, index) {
